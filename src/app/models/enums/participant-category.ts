60e7c30e050f1a3fb5891230bf08b8fde530bdff
Luan Martins <muanlartins@gmail.com>
2024-10-14 17:50:23 -0300

    feat: schools category

--- src/app/models/enums/participant-category.ts
+++ src/app/models/enums/participant-category.ts
@@ -1,4 +1,5 @@
 export enum ParticipantCategory {
+  schools = 'schools',
   novice = 'novice',
   open = 'open',
   dino = 'dino'
@@ -6,6 +7,8 @@ export enum ParticipantCategory {
 
 export function getParticipantCategoryViewValue(participantCategory: ParticipantCategory) {
   switch(participantCategory) {
+    case ParticipantCategory.schools:
+      return 'Schools';
     case ParticipantCategory.novice:
       return 'Novice';
     case ParticipantCategory.open:
