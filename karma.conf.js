a9beced825dbbb7fe7e3b2ff6fc10fdc62da1de0
Luan Martins <muanlartins@gmail.com>
2024-10-15 14:03:05 -0300

    feat: update backend url

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tab-deb'),
+      dir: require('path').join(__dirname, './coverage/agora'),
       subdir: '.',
       reporters: [
         { type: 'html' },
