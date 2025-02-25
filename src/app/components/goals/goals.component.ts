2b152e27de9640b57de3a4b117f27a00007c5a6e
muanlartins <muanlartins@gmail.com>
2025-02-25 11:18:53 -0300

    feat: goals page mobile compatibility

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -30,141 +30,34 @@ export class GoalsComponent implements OnInit {
       this.competitionGoals = this.goals.filter((goal: Goal) => goal.type === 'competition');
       this.managementGoals = this.goals.filter((goal: Goal) => goal.type === 'management');
     });
-
-    // this.competitionGoals = [
-    //   {
-    //     title: 'Ganhar 1 Título Major',
-    //     currentCount: 2,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Realizar 3 Breaks Major',
-    //     currentCount: 5,
-    //     totalCount: 3,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Ganhar 3 Títulos Open',
-    //     currentCount: 3,
-    //     totalCount: 3,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Alcançar 10 Finais Open',
-    //     currentCount: 11,
-    //     totalCount: 10,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Realizar 10 Breaks Open',
-    //     currentCount: 18,
-    //     totalCount: 10,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Ganhar 3 Títulos Novice',
-    //     currentCount: 2,
-    //     totalCount: 3,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Alcançar 5 Finais Novice',
-    //     currentCount: 4,
-    //     totalCount: 5,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Realizar 5 Breaks Novice',
-    //     currentCount: 4,
-    //     totalCount: 5,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Participar de 5 CA Teams',
-    //     currentCount: 4,
-    //     totalCount: 5,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Realizar 10 Breaks de Juízes',
-    //     currentCount: 9,
-    //     totalCount: 10,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Alcançar 10 vezes Top 10 em campeonatos na categoria Open',
-    //     currentCount: 17,
-    //     totalCount: 10,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Alcançar 10 vezes Top 10 em campeonatos na categoria Novice',
-    //     currentCount: 12,
-    //     totalCount: 10,
-    //     achieved: false,
-    //   },
-    // ];
-
-
-
-    // this.managementGoals = [
-    //   {
-    //     title: 'Levar uma dupla da Sociedade de Debates da UFRJ para o WUDC Panamá 2024',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Possuir uma delegação em todos os campeonatos estaduais, regionais e nacional do ano',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Conseguir que a SDUFRJ se torne um Projeto de Extensão',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Realizar ao menos um Debate Misto com cada Sociedade de Debates registrada no Brasil',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Mínimo de 4 Equipes para o Nacional (2° Semestre)',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Patrocínio Particular para Campeonatos',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    //   {
-    //     title: 'Banco de Case Files',
-    //     currentCount: 0,
-    //     totalCount: 1,
-    //     achieved: false,
-    //   },
-    // ];
   }
 
   public createGoal() {
-    this.dialog.open(CreateGoalModalComponent, { width: '70vw', maxHeight: '80vh', autoFocus: false, disableClose: true });
+    this.dialog.open(CreateGoalModalComponent, { 
+      minWidth: 'calc(100vw - 2rem)',
+      minHeight: 'calc(100vh - 2rem)',
+      maxHeight: 'calc(100vh - 2rem)',
+      maxWidth: 'calc(100vw - 2rem)',
+      autoFocus: false, 
+      disableClose: true 
+    });
   }
 
   public editGoal(id: string, event: Event) {
     event.stopPropagation();
 
-    this.dialog.open(CreateGoalModalComponent, { width: '70vw', maxHeight: '80vh', autoFocus: false, disableClose: true, data: {
-      isEditing: true,
-      goal: this.goals.find((goal) => goal.id === id)
-    }});
+    this.dialog.open(CreateGoalModalComponent, { 
+      minWidth: 'calc(100vw - 2rem)',
+      minHeight: 'calc(100vh - 2rem)',
+      maxHeight: 'calc(100vh - 2rem)',
+      maxWidth: 'calc(100vw - 2rem)',
+      autoFocus: false, 
+      disableClose: true, 
+      data: {
+        isEditing: true,
+        goal: this.goals.find((goal) => goal.id === id)
+      }
+    });
   }
 
   public deleteGoal(id: string, event: Event) {
@@ -172,12 +65,19 @@ export class GoalsComponent implements OnInit {
 
     const goal = this.goals.find((goal) => goal.id === id)!;
 
-    this.dialog.open(ConfirmModalComponent, { data: {
-      text: `Você tem certeza que quer deletar o artigo <b>${goal.title}</b>?`,
-      positiveCallback: async () => {
-        await this.goalService.deleteGoal(id);
-      }
-    } });
+    this.dialog.open(ConfirmModalComponent, { 
+      minWidth: 'calc(100vw - 2rem)',
+      minHeight: 'calc(100vh - 2rem)',
+      maxHeight: 'calc(100vh - 2rem)',
+      maxWidth: 'calc(100vw - 2rem)',
+      data: {
+        text: `Você tem certeza que quer deletar o artigo <b>${goal.title}</b>?`,
+        positiveCallback: async () => {
+          await this.goalService.deleteGoal(id);
+        },
+        negativeCallback: async () => {}
+      } 
+    });
   }
 
   public isAdmin() {
